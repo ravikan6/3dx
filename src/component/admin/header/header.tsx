fe2c910b496be95d098545cef3cfde20b5f9d5a1
Krishnachavan1930 <krishnachavan459@gmail.com>
2025-01-02 16:43:55 +0530

    admin dashboad updated

--- src/component/admin/header/header.tsx
+++ src/component/admin/header/header.tsx
@@ -7,12 +7,12 @@ export function Header() {
     <div className="flex items-center justify-between p-4 border-b">
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2">
-        <Button variant="ghost" size="icon" className="h-9 w-9">
+        {/* <Button variant="ghost" size="icon" className="h-9 w-9">
           <Bell className="h-4 w-4" />
-        </Button>
-        <Button size="sm" className="bg-[#FF5A1F] hover:bg-[#FF5A1F]/90 text-white mr-0">
+        </Button> */}
+        {/* <Button size="sm" className="bg-[#FF5A1F] hover:bg-[#FF5A1F]/90 text-white mr-0">
           Export
-        </Button>
+        </Button> */}
       </div>
       </div>
     </div>
